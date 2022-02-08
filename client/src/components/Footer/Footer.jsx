961dde40a631364bb477244ba662e52dd231d803
Aakash Jha <aakash.jha1103@gmail.com>
2022-02-09 01:03:11 +0530

    CSS issues fixed

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -21,8 +21,6 @@ const Footer = () => {
 						rel="noreferrer">
 						<svg
 							xmlns="http://www.w3.org/2000/svg"
-							width="100%"
-							height="100%"
 							fill="currentColor"
 							className="bi bi-github"
 							viewBox="0 0 16 16">
