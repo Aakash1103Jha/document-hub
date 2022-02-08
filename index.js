b62d1b37683c6ddb4707e8ab75df8edee558b22d
Aakash Jha <aakash.jha1103@gmail.com>
2022-02-08 23:58:30 +0530

    Fixed - width issue
    fixed issue where elements on page oved when scroll bar appeared

--- index.js
+++ index.js
@@ -12,9 +12,9 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
-app.use(express.static(path.resolve(__dirname, "client", "build")))
+app.use(express.static(path.resolve(__dirname, "build")))
 app.get("*", (req, res) => {
-	res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+	res.sendFile(path.resolve(__dirname, "build", "index.html"))
 })
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
