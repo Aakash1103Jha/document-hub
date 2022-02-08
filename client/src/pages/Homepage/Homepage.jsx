e375278581248300859a43a970e4d8122a048c0d
Aakash Jha <aakash.jha1103@gmail.com>
2022-02-09 02:22:39 +0530

    content in homepage
    - intro content in homepage
    - moved github logo from footer to navbar
    - other minor css changes

--- client/src/pages/Homepage/Homepage.jsx
+++ client/src/pages/Homepage/Homepage.jsx
@@ -8,6 +8,19 @@ const Homepage = () => {
 				<h1>documentHub</h1>
 				<p>Documentations for developers, by developers</p>
 			</div>
+			<div className={styles.row}>
+				<h2 className={styles.h2}>Getting started</h2>
+				<p className={styles.p}>
+					Register as a contributor to get started with adding your own documents for
+					others to use. Viewing and downloading documents, on the other hand, needs no
+					registration.
+				</p>
+				<br />
+				<p className={styles.p}>
+					In case you see something that isn't working the way it should, feel free to
+					report it using the Report Issue link.
+				</p>
+			</div>
 		</div>
 	)
 }
