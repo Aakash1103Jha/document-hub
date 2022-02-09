e60bd4c7eae4a6264528022aa436f04aad9f7126
Aakash Jha <aakash.jha1103@gmail.com>
2022-02-10 02:00:53 +0530

    Register and Validate User
    - added proper logic to `onRegister`
    - sending jwt as cookie
    - using `validateToken` middleware to check jtw validity
    - modified `validatePassword` with regex
    - added testcase for password

--- server/controllers/User.controller.js
+++ server/controllers/User.controller.js
@@ -1,15 +1,16 @@
 const User = require("../models/User")
-const { compare, genSalt } = require("bcrypt")
+const { compare, genSalt, hash } = require("bcrypt")
 const { sign } = require("jsonwebtoken")
+const { validatePassword } = require("../helpers/validation")
 
 const onLogin = async (req, res) => {
-	// if (!req.body) return res.status(500).json("Invalid input")
+	if (!req.body) return res.status(400).json("Invalid input")
 	const { email, password } = req.body
 	try {
 		const user = await User.findOne({ email: email })
-		if (!user) return res.status(404).json("User not found")
+		if (!user) return res.status(401).json("User not found")
 		const validPassword = await compare(password, user.password)
-		if (!validPassword) res.status(403).json("Invalid password")
+		if (!validPassword) res.status(401).json("Invalid password")
 		const token = sign({ _id: user._id }, process.env.TOKEN_SECRET, {
 			expiresIn: "1d",
 			noTimestamp: false,
@@ -18,13 +19,35 @@ const onLogin = async (req, res) => {
 			.cookie("token", token, {
 				httpOnly: true,
 				signed: true,
-				expires: new Date(Date.now() + 86400),
+				expires: new Date(Date.now() + 86400000),
 			})
 			.json("Logged In")
 	} catch (err) {
 		return res.status(500).json({ Error: err.message })
 	}
 }
-const onRegister = async (req, res) => {}
+const onRegister = async (req, res) => {
+	if (!req.body) return res.status(401).json("Invalid input")
+	const { email, password } = req.body
+	try {
+		const user = await User.findOne({ email: email })
+		if (user) return res.status(401).json("Email already in use")
+		if (validatePassword(password) === false)
+			return res
+				.status(401)
+				.json(
+					"Password must contain at least 1 uppercase letter, number and special character.",
+				)
+		const hashPassword = await hash(password, await genSalt(10))
+		const newUser = new User({ ...req.body, password: hashPassword })
+		await newUser.save()
+		return res.status(200).json("Successfully registered 🚀")
+	} catch (err) {
+		return res.status(500).json({ Error: err.message })
+	}
+}
+const onGetProfile = async (req, res) => {
+	// TODO: User profile
+}
 
-module.exports = { onLogin, onRegister }
+module.exports = { onLogin, onRegister, onGetProfile }
--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const cors = require("cors")
 const path = require("path")
 const cookieParser = require("cookie-parser")
 
-const { startServer } = require("./helpers/connection")
+const { startServer, connectToDb } = require("./helpers/connection")
 
 const app = express()
 
@@ -19,11 +19,12 @@ app.use(cookieParser(process.env.COOKIE_SECRET))
 // for production deployment
 app.use(express.static(path.resolve(__dirname, "..", "dist", "build")))
 
-app.get("/api", (req, res) => {
-	const cookie = req.signedCookies
-	console.log({ cookie })
-	res.json("🚀")
-})
+const UserRoute = require("./routes/User.route")
+const DocumentRoute = require("./routes/Document.route")
+
+app.use("/auth/", UserRoute)
+app.use("/doc/", DocumentRoute)
+
 app.get("*", (req, res) => {
 	// for local deployment
 	// res.sendFile(path.resolve(__dirname, "..", "client", "build", "index.html"))
@@ -31,10 +32,5 @@ app.get("*", (req, res) => {
 	res.sendFile(path.resolve(__dirname, "..", "dist", "build", "index.html"))
 })
 
-const UserRoute = require("./routes/User.route")
-const DocumentRoute = require("./routes/Document.route")
-
-app.use("/auth/", UserRoute)
-app.use("/", DocumentRoute)
-
+connectToDb()
 startServer(app)
